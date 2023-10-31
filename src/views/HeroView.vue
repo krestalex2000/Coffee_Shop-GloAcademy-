db0e822efbd26e4874e2c8b8b92c09ef30752047
Alex Krestyannikov <avocodefreetrial@yandex.ru>
2023-10-31 20:40:28 +0500

    создал объект date для страниц, добавил объекты товаров, вывел динамически link в NavBar`e

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -67,7 +67,6 @@
         </div>
       </div>
     </section>
-    
   </main>
 </template>
 
@@ -76,6 +75,28 @@ import NavBarComponent from '@/components/NavBarComponent.vue'
 import CardItemComponent from '@/components/CardItemComponent.vue'
 
 export default {
+  data() {
+    return [
+      {
+        id: 0,
+        icon: 'coffee-1.jpg',
+        title: 'Solimo Coffee Beans 2kg',
+        price: '10.73$'
+      },
+      {
+        id: 1,
+        icon: 'coffee-2.jpg',
+        title: 'Presto Coffee Beans 1kg',
+        price: '15.99$'
+      },
+      {
+        id: 2,
+        icon: 'coffee-3.jpg',
+        title: 'AROMISTICO Coffee 1kg',
+        price: '6.99$'
+      },
+    ]
+  },
   components: {NavBarComponent, CardItemComponent}
 }
 </script>
